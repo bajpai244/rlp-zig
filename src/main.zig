d255237dca2cbb0d7760773a6dee2965bde3fd31
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-01-27 15:42:42 +0530

    feat: complete initial test suite

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const testing = std.testing;
 
 const RLPItem = union(enum) {
-    String: []const u8,
-    List: []const RLPItem,
+    String: []u8,
+    List: []RLPItem,
 };
 
 const RLPItemType = enum {
@@ -76,59 +76,88 @@ fn encode_rlp(input: RLPItem) !std.ArrayList(u8) {
     return rlp_encoding;
 }
 
+fn parseUnsignedFromBytes(input: []u8) usize {
+    var result: usize = 0;
+    for (input, 0..) |byte, idx| {
+        var byte_usize: usize = @intCast(byte);
+        result += byte_usize << @intCast((8 * idx));
+    }
+
+    return result;
+}
+
+const DecodedItemTypeAndLength = struct {
+    itemType: RLPItemType,
+    itemIdx: usize,
+    itemLength: usize,
+};
+
 /// retrieve the type, the index from where the payload starts and the length of the payload
-fn decodeItemTypeAndLength(input: []u8) !struct { RLPItemType, usize, usize } {
+fn decodeItemTypeAndLength(input: []u8) !DecodedItemTypeAndLength {
     const first_byte = input[0];
     if (first_byte >= 0 and first_byte <= 0xbf) {
         if (first_byte <= 0x7f) {
-            return struct { RLPItemType.String, 0, 1 };
+            return DecodedItemTypeAndLength{
+                .itemType = RLPItemType.String,
+                .itemIdx = 0,
+                .itemLength = 1,
+            };
         } else if (first_byte <= 0xb7) {
-            return struct { RLPItemType.String, 1, (first_byte - 0x80) };
+            return DecodedItemTypeAndLength{ .itemType = RLPItemType.String, .itemIdx = 1, .itemLength = (first_byte - 0x80) };
         } else {
-            const len_bytes = input[1 .. first_byte - 0xb7];
-            const len = std.mem.fromBytes(usize, len_bytes);
-            return struct { RLPItemType.String, (1 + len_bytes.len), len };
+            const len_bytes = input[1 .. (first_byte - 0xb7) + 1];
+            const len = parseUnsignedFromBytes(len_bytes);
+            std.debug.print("I do come here {} \n", .{len});
+            return DecodedItemTypeAndLength{ .itemType = RLPItemType.String, .itemIdx = (1 + len_bytes.len), .itemLength = len };
         }
     } else {
         if (first_byte <= 0xf7) {
-            return struct { RLPItemType.List, 1, (first_byte - 0xc0) };
+            return DecodedItemTypeAndLength{ .itemType = RLPItemType.List, .itemIdx = 1, .itemLength = first_byte - 0xc0 };
         } else {
-            const len_bytes = input[1 .. first_byte - 0xf7];
-            const len = std.mem.fromBytes(usize, len_bytes);
-            return struct { RLPItemType.List, (1 + len_bytes.len), len };
+            const len_bytes = input[1 .. (first_byte - 0xf7) + 1];
+            const len = parseUnsignedFromBytes(len_bytes);
+            return DecodedItemTypeAndLength{ .itemType = RLPItemType.List, .itemIdx = (1 + len_bytes.len), .itemLength = len };
         }
     }
 }
 
-fn decode_rlp(input: []u8) !std.ArrayList(RLPItem) {
-    var rlp_decoding = std.ArrayList(RLPItem).init(std.testing.allocator);
-
+fn decode_rlp(input: []u8) !RLPItem {
     var index: usize = 0;
-    while (index < input.len) {
-        const result = try decodeItemTypeAndLength(input[index..]);
-        const item_type = result[0];
-        const item_idx = result[1];
-        const item_length = result[2];
+    var item: RLPItem = undefined;
 
-        var item: RLPItem = undefined;
-        if (item_type == RLPItemType.String) {
-            item = RLPItem{ .String = input[index + item_idx .. index + item_idx + item_length] };
-        } else {
-            item = RLPItem{ .List = try decode_rlp(input[index + item_idx .. index + item_idx + item_length]) };
+    const result = try decodeItemTypeAndLength(input[index..]);
+    const item_type = result.itemType;
+    const item_idx = result.itemIdx;
+    const item_length = result.itemLength;
+
+    if (item_type == RLPItemType.String) {
+        item = RLPItem{ .String = input[index + item_idx .. index + item_idx + item_length] };
+    } else {
+        var rlp_items = std.ArrayList(RLPItem).init(std.testing.allocator);
+        defer rlp_items.deinit();
+
+        while (index < input.len) {
+            if (item_type == RLPItemType.String) {
+                item = RLPItem{ .String = input[index + item_idx .. index + item_idx + item_length] };
+            } else {
+                var rlp_item = try decode_rlp(input[index + item_idx .. index + item_idx + item_length]);
+                try rlp_items.append(rlp_item);
+            }
+
+            index += item_idx + item_length;
         }
 
-        try rlp_decoding.append(item);
-        index += item_idx + item_length;
+        item = RLPItem{ .List = try rlp_items.toOwnedSlice() };
     }
 
-    return rlp_decoding;
+    return item;
 }
 
 fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
-test "encode_rlp_single_byte" {
+test "encode_rlp_string_single_byte" {
     var inputBytes = std.ArrayList(u8).init(std.testing.allocator);
     defer inputBytes.deinit();
 
@@ -142,7 +171,7 @@ test "encode_rlp_single_byte" {
     std.debug.assert(result.items[0] == 42);
 }
 
-test "encode_rlp_less_than_55_byte" {
+test "encode_rlp_string_less_than_55_byte" {
     var inputBytes = std.ArrayList(u8).init(std.testing.allocator);
     defer inputBytes.deinit();
 
@@ -186,7 +215,9 @@ test "encode_rlp_list_less_than_56_bytes" {
     try inputBytes.append(42);
     try inputBytes.append(32);
 
-    var input = RLPItem{ .List = &[_]RLPItem{RLPItem{ .String = inputBytes.items }} };
+    var arr = [_]RLPItem{RLPItem{ .String = inputBytes.items }};
+    var runtime_known_zero: usize = 0;
+    var input = RLPItem{ .List = arr[runtime_known_zero..] };
 
     var expected = std.ArrayList(u8).init(std.testing.allocator);
     defer expected.deinit();
@@ -210,7 +241,9 @@ test "encode_rlp_list_more_than_55_bytes" {
         try inputBytes.append(@intCast(i));
     }
 
-    var input = RLPItem{ .List = &[_]RLPItem{RLPItem{ .String = inputBytes.items }} };
+    var arr = [_]RLPItem{RLPItem{ .String = inputBytes.items }};
+    var runtime_known_zero: usize = 0;
+    var input = RLPItem{ .List = arr[runtime_known_zero..] };
 
     var expected = std.ArrayList(u8).init(std.testing.allocator);
     defer expected.deinit();
@@ -225,4 +258,79 @@ test "encode_rlp_list_more_than_55_bytes" {
     defer result.deinit();
 
     std.debug.assert(std.mem.eql(u8, result.items, expected.items));
+}
+
+test "decode_rlp_single_byte" {
+    var rlp = std.ArrayList(u8).init(std.testing.allocator);
+    defer rlp.deinit();
+    try rlp.append(42);
+
+    var result = try decode_rlp(rlp.items);
+    std.debug.assert(result.String[0] == 42);
+}
+
+test "decode_rlp_string_less_than_55_bytes" {
+    var rlp = std.ArrayList(u8).init(std.testing.allocator);
+    defer rlp.deinit();
+
+    try rlp.append(0x80 + 2);
+    try rlp.append(42);
+    try rlp.append(32);
+
+    var result = try decode_rlp(rlp.items);
+    std.debug.assert(result.String[0] == 42);
+    std.debug.assert(result.String[1] == 32);
+}
+
+test "decode_rlp_string_more_than_55_bytes" {
+    var rlp = std.ArrayList(u8).init(std.testing.allocator);
+    defer rlp.deinit();
+
+    try rlp.append(0xb7 + 1);
+    try rlp.append(56);
+    for (0..56) |i| {
+        try rlp.append(@intCast(i));
+    }
+
+    var result = try decode_rlp(rlp.items);
+
+    for (0..56) |i| {
+        std.debug.assert(result.String[i] == @as(u8, @intCast(i)));
+    }
+}
+
+test "decode_rlp_list_less_than_55_bytes" {
+    var rlp = std.ArrayList(u8).init(std.testing.allocator);
+    defer rlp.deinit();
+
+    try rlp.append(0xc0 + 3);
+    try rlp.append(0x80 + 2);
+    try rlp.append(42);
+    try rlp.append(32);
+
+    var result = try decode_rlp(rlp.items);
+    defer std.testing.allocator.free(result.List);
+
+    std.debug.assert(result.List[0].String[0] == 42);
+    std.debug.assert(result.List[0].String[1] == 32);
+}
+
+test "decode_rlp_list_more_than_55_bytes" {
+    var rlp = std.ArrayList(u8).init(std.testing.allocator);
+    defer rlp.deinit();
+
+    try rlp.append(0xf7 + 1);
+    try rlp.append(58);
+    try rlp.append(0xb7 + 1);
+    try rlp.append(56);
+    for (0..56) |i| {
+        try rlp.append(@intCast(i));
+    }
+
+    var result = try decode_rlp(rlp.items);
+    defer std.testing.allocator.free(result.List);
+
+    for (0..56) |i| {
+        std.debug.assert(result.List[0].String[i] == @as(u8, @intCast(i)));
+    }
 }
